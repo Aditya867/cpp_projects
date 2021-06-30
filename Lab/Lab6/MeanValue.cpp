0e3435d863f05092dc909f555d0d3b9e15746176
Aditya <nannani7890@gmail.com>
2021-06-30 07:14:54 -0600

    Familiarization with friend function

---
+++ Lab/Lab6/MeanValue.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class Sample{
+  float num1,num2;
+
+  public:
+    Sample(){}
+    Sample(float num1,float num2){
+      this->num1=num1;
+      this->num2=num2;
+    }
+    void getdata(){
+      cout<<"Enter two decimal numbers :"<<endl;
+      cin>>num1>>num2;
+    }
+    friend float mean(Sample s);
+    
+    ~Sample(){}
+
+};
+
+float mean(Sample s){
+  return (s.num1+s.num2)/2;
+}
+
+int main(){
+  Sample s1;
+  s1.getdata();
+  cout<<"Average is :"<<mean(s1)<<endl;
+  system("pause");
+  return 0;
+}
